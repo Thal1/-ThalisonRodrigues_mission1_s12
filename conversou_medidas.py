1bec92ba40b30c4ab1dbff36ccb6257dd83790ff
Thalison Rodrigues <thalissonrodrigues0101@gmail.com>
2024-06-05 18:08:17 -0300

    conversou alterado

--- conversou_medidas.py
+++ conversou_medidas.py
@@ -1,9 +1,14 @@
-class conversorMedidas:
+class ConversorMedidas:
     def _init_(self):
         pass
 
-    def converter_cm_para_m():
-        pass
+    def converter_cm_para_m(valor_cm):
+        valor_metros = valor_cm / 100
+        return valor_metros
 
     def converter_m_para_cm():
-        pass        
+        pass 
+
+        
+resultado= ConversorMedidas.converter_cm_para_m(100)
+print('Resultado:',resultado)        
