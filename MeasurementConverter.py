e09153442d48c3bc0ce995960932ea3860d550f7
Thalison Rodrigues <thalissonrodrigues0101@gmail.com>
2024-06-19 17:32:13 -0300

    conversoumedidas

---
+++ MeasurementConverter.py
@@ -0,0 +1,9 @@
+class MeasurementConverter:
+    def __init__():
+        pass
+
+    def centimeters_to_meters(value_cm):
+        return value_cm / 100
+
+    def meters_to_centimeters(value_meters):
+        return value_meters * 100
